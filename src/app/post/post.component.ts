5911b6f7d53d8cf4c39c1f22d43d30e9771a2912
gustavomr <gustavomr@gmail.com>
2018-03-08 19:58:46 -0300

    adicionando primeng, bootstrap, fontawesome

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {PostService} from './service/post.service';
 import { Post } from './post';
+import { DataTableModule } from 'primeng/primeng';
+
 
 
 @Component({
